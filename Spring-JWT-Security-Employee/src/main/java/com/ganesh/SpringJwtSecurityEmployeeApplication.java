0ef8945be005533fbd12c17bdc634564e6c30294
Ganesh <ganeshgarje716@gmail.com>
2026-07-23 23:40:29 +0530

    Create Spring JWT Security Employee

---
+++ Spring-JWT-Security-Employee/src/main/java/com/ganesh/SpringJwtSecurityEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtSecurityEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtSecurityEmployeeApplication.class, args);
+	}
+
+}
