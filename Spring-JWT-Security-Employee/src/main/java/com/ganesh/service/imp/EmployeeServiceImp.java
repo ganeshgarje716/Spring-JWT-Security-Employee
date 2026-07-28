bf470c12979c9e0173ce5dd284a2d11acedc14e8
Ganesh <ganeshgarje716@gmail.com>
2026-07-28 23:09:57 +0530

    Create EmployeeServiceImp Class

---
+++ Spring-JWT-Security-Employee/src/main/java/com/ganesh/service/imp/EmployeeServiceImp.java
@@ -0,0 +1,10 @@
+package com.ganesh.service.imp;
+
+import org.springframework.stereotype.Service;
+
+import com.ganesh.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImp implements EmployeeService{
+
+}
