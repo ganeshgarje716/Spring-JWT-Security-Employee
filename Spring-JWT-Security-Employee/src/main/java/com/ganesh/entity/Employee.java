4c22e4c3eb0ae5ae0cd68c6ee7a311a2da3e8ba9
Ganesh <ganeshgarje716@gmail.com>
2026-07-30 23:15:42 +0530

    Create Variables

--- Spring-JWT-Security-Employee/src/main/java/com/ganesh/entity/Employee.java
+++ Spring-JWT-Security-Employee/src/main/java/com/ganesh/entity/Employee.java
@@ -1,6 +1,13 @@
 package com.ganesh.entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +17,23 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 public class Employee {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	private Double salary;
+	
+	private String department;
+	
+	private String address;
+	
+	private String mobileNo;
+	
+	@CreationTimestamp
+	private LocalDateTime joiningdate;
 
 }
