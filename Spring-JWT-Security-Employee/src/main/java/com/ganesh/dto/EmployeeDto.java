7017b0f8ca9fa1d3523d9f7d5adc3ef415f46fc8
Ganesh <ganeshgarje716@gmail.com>
2026-07-31 23:27:17 +0530

    Add Validations Into the Variables

--- Spring-JWT-Security-Employee/src/main/java/com/ganesh/dto/EmployeeDto.java
+++ Spring-JWT-Security-Employee/src/main/java/com/ganesh/dto/EmployeeDto.java
@@ -1,8 +1,35 @@
 package com.ganesh.dto;
 
+import java.time.LocalDateTime;
+
+
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class EmployeeDto {
+	
+	
+	
+	private Integer id;
+	
+	@NotNull(message = "Name Is Required")
+	private String name;
+	
+	@NotNull(message = "Salary Is Required")
+	private Double salary;
+	
+	@NotNull(message = "Department Is Required")
+	private String department;
+	
+	@NotNull(message = "Address Is Required")
+	private String address;
+	
+	@NotNull(message = "Mobile No Is Required")
+	private String mobileNo;
+	
+	private LocalDateTime joiningdate;
+	
+	
 
 }
